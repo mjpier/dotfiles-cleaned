7668e365f069f11604baa3149bcc7d1226a85e8d
TruncatedDinosour <asnojus039@gmail.com>
2021-06-04 21:59:46 +0300

    simplified the FISH shell, update the FISH functions

--- dotfiles/suckless/dwm/dwm-6.2/config.h
+++ dotfiles/suckless/dwm/dwm-6.2/config.h
@@ -37,6 +37,7 @@ static const Rule rules[] = {
     { "ksnip",                      NULL,       NULL,       0,            1,            0,          1,          -1 },
     { "st-256color",                NULL,       NULL,       0,            0,            1,          0,          -1 },
     { "Alacritty",                  NULL,       NULL,       0,            0,            1,          0,          -1 },
+    { NULL,                         NULL,   "Event Tester", 0,            0,            0,          1,          -1 }, /* xev */
 
     /* tag 2 */
     { "firefox",                    NULL,       NULL,       1 << 1,       0,           0,           0,          -1 },
